a51cf593d56e7843519fc748a5963250b3a48484
Sunilsingh3233 <sunilsingh.bkbiet2022@gmail.com>
2026-08-17 10:51:09 +0530

    Featured Section added

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,11 +1,24 @@
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const FeaturedSection = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      1:13
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
+      <div className='relative flex items-center pt-20 justify-between pb-10'>
+        <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
+        <button onClick={()=>navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300'>view All <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5'/></button>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
 
 export default FeaturedSection
+
+
+// 1:17
