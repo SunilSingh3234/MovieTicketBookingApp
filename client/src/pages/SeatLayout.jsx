0b64a7cdf315624bd043aa219eab3b962fd429cc
Sunilsingh3233 <sunilsingh.bkbiet2022@gmail.com>
2026-08-22 11:01:40 +0530

    adding my-booking page

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { assets, dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
-import { ClockIcon } from 'lucide-react'
+import { ArrowRightIcon, ClockIcon } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import BlurCircle from '../components/BlurCircle'
+import toast from 'react-hot-toast'
+
 
 const SeatLayout = () => {
+
+  const groupRows = [["A", "B"], ["C", "D"], ["E", "F"], ["G", "H"], ["I", "J"]]
 
   const { id, date } = useParams()
   const [selectedSeats, setSelectedSeats] = useState([])
@@ -23,6 +27,29 @@ const SeatLayout = () => {
       })
     }
   }
+
+  const handleSeatClick = (seatId) => {
+    if (!selectedTime) {
+      return toast("Please select time first")
+    }
+    if (!selectedSeats.includes(seatId) && selectedSeats.length > 4) {
+      return toast("You can only select 5 seats")
+    }
+    setSelectedSeats(prev => prev.includes(seatId) ? prev.filter(seat => seat !== seatId) : [...prev, seatId])
+  }
+
+  const renderSeats = (row, count = 9) => (
+    <div className="flex gap-2 mt-2" key={row}>
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {Array.from({ length: count }, (_, i) => {
+          const seatId = `${row}${i + 1}`;
+          return (
+            <button key={seatId} onClick={() => handleSeatClick(seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && "bg-primary text-white"}`}>{seatId}</button>
+          )
+        })}
+      </div>
+    </div>
+  )
 
   useEffect(() => {
     getShow()
@@ -43,11 +70,24 @@ const SeatLayout = () => {
       </div>
 
       <div className='relative flex flex-1 flex-col max-md:mt-16 items-center'>
-          <BlurCircle top='-100px' left='-100px'/>
-          <BlurCircle bottom='0px' right='0px'/>
-          <h1 className='text-2xl font-semibold mb-4'>Select Your Seats</h1>
-          <img src={assets.screenImage} alt="screen" />
-          <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
+        <BlurCircle top='-100px' left='-100px' />
+        <BlurCircle bottom='0px' right='0px' />
+        <h1 className='text-2xl font-semibold mb-4'>Select Your Seats</h1>
+        <img src={assets.screenImage} alt="screen" />
+        <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
+        <div className='flex flex-col mt-10 items-center text-xs text-gray-300'>
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+            {groupRows[0].map(row => renderSeats(row))}
+          </div>
+          <div className="grid grid-cols-2 gap-11">
+            {
+              groupRows.slice(1).map((group, idx) => (
+                <div key={idx}>{group.map(row => renderSeats(row))}</div>
+              ))
+            }
+          </div>
+        </div>
+            <button onClick={()=>navigate('/my-bookings')} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95'>Proceed To Checkout <ArrowRightIcon className='h-4 w-4' strokeWidth={3}/></button>
       </div>
     </div>
   ) : (<Loading />)
@@ -55,4 +95,3 @@ const SeatLayout = () => {
 
 export default SeatLayout
 
-// 2:53
